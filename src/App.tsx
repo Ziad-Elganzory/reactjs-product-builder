b13282989a3ace1749d190bd0b570f3a10b48fbc
Ziad-Elganzory <ziadmohamed770@outlook.com>
2025-06-18 14:53:15 +0300

    Use Headless UI for modal

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,16 @@
 import ProductCard from "./components/ProductCard"
+import Modal from "./components/ui/Modal";
 import { productList } from "./data"
 
 const App = () => {
   const renderProductList = productList.map(product => <ProductCard key={product.id} product={product} />);
   return (
-    <div className="container">
+    <main className="container">
       <div className="m-5 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-2 md:gap-4 p-2 rounded-md">
         {renderProductList}
       </div>
-    </div>
+      <Modal/>
+    </main>
   )
 }
 
